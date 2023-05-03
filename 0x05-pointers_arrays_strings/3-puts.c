5cd06143cddd7edb3cfd3e8498c11074c9c1c89e
nada <nadamaghraby93@gmail.com>
2023-05-02 18:21:44 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts - prints a string
+ *@str: the string
+ *Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
