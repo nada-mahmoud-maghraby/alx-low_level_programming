8d73c4a674d847459676a944c79ef9ceb702eefe
nada <nadamaghraby93@gmail.com>
2023-05-02 20:04:37 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,11 +6,19 @@
  */
 void puts_half(char *str)
 {
-	int i;
+	int j = 0;
+	int k;
 
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	for (i /= 2; str[i] != '\0'; i++)
-		_putchar(str[i]);
+	while (str[j] != '\0')
+		j++;
+	if (j % 2 == 1)
+	{
+		k = (j - 1) / 2;
+		k += 1;
+	}
+	else
+		k = j / 2;
+	for ( ; k < j; k++)
+		_putchar(str[k]);
 	_putchar('\n');
 }
