3bac2dfc865b664f2ceb6c7fcc81efae56c2d507
nada <nadamaghraby93@gmail.com>
2023-05-02 18:41:10 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *rev_string - reverses a string.
+ *@s: is a string
+ *Return: void
+ */
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	for (l = 0; s[l] != '\0'; ++l)
+		;
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
+}
