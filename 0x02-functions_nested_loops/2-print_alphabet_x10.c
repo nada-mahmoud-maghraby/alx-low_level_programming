e9bc731e91d98a88181875916c4479cf6149bc7a
nada <nadamaghraby93@gmail.com>
2023-05-09 17:01:12 -0700

    task 1,2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print all alphabet in lowercase ten times
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
